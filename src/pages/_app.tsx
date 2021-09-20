84e2c978e6c1c8529699dbd6ff5c869bf95fd12b
Pon <tyanpon57@gmail.com>
2021-09-20 14:52:19 +0900

    max-width 設定した

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,24 +7,26 @@ import Link from 'next/link'
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
-            <header className={styles.heading}>
-                <h1>
-                    <Link href="/">
-                        <a className={styles.brand}>ぽんブログ</a>
+            <div className={styles.site}>
+                <header className={styles.heading}>
+                    <h1>
+                        <Link href="/">
+                            <a className={styles.brand}>ぽんブログ</a>
+                        </Link>
+                    </h1>
+                </header>
+                <div className={styles.content}>
+                    <Component {...pageProps} />
+                </div>
+                <footer className={styles.footer}>
+                    © 2021{' '}
+                    <Link href="https://twitter.com/pon_dev">
+                        <a target="_blank" rel="noreferrer">
+                            Pon
+                        </a>
                     </Link>
-                </h1>
-            </header>
-            <div className={styles.content}>
-                <Component {...pageProps} />
+                </footer>
             </div>
-            <footer className={styles.footer}>
-                © 2021{' '}
-                <Link href="https://twitter.com/pon_dev">
-                    <a target="_blank" rel="noreferrer">
-                        Pon
-                    </a>
-                </Link>
-            </footer>
         </>
     )
 }
