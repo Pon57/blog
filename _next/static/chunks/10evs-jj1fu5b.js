114a2ba10a1bf8918addd78f6a392743c7958ee0
Pon57 <Pon57@users.noreply.github.com>
2026-04-10 23:04:09 +0000

    deploy: d7885f0043c05343aaa707a4120985b51bf531e3

---
+++ _next/static/chunks/10evs-jj1fu5b.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/disclaimer", [
+  "static/chunks/0yuvj0sclk2jp.js",
+  "static/chunks/0v~g5_81ots--.js",
+  "static/chunks/0xh5u6wvb-059.js",
+  "static/chunks/03t7duw_g37c9.js",
+  "static/chunks/16ja3sn.h~jqz.css",
+  "static/chunks/turbopack-0yvsfx38mi3am.js"
+])
