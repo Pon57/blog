29e526346560736b1827e8ac529da53811a1e81e
Pon57 <Pon57@users.noreply.github.com>
2026-04-26 14:51:36 +0000

    deploy: f95a97aa8d103296198a8629196b5f867ec55a75

--- _next/static/chunks/11m.5btwsvtou.js
+++ _next/static/chunks/11m.5btwsvtou.js
@@ -3,6 +3,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/029ipua9607~f.js",
   "static/chunks/0.byjbzm10ire.js",
   "static/chunks/03fw1cqy6aj~v.js",
-  "static/chunks/0519bwhrrp~_q.css",
-  "static/chunks/turbopack-0zhh_z~otajrm.js"
+  "static/chunks/0v7m~0qt~~zf8.css",
+  "static/chunks/turbopack-0q57ossealt-4.js"
 ])
