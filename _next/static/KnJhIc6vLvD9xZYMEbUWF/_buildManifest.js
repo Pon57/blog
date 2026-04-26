4ffd040038373d0b56bf533431fdb41b64e057a7
Pon57 <Pon57@users.noreply.github.com>
2026-04-26 08:52:32 +0000

    deploy: 6969e5e49125fc639c3725b394aead05ef196b09

--- _next/static/KnJhIc6vLvD9xZYMEbUWF/_buildManifest.js
+++ _next/static/KnJhIc6vLvD9xZYMEbUWF/_buildManifest.js
@@ -1,21 +1,21 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/0gs.~xaoai-3k.js"
+    "static/chunks/03feem9fw-07-.js"
   ],
   "/_error": [
     "static/chunks/03m6q1v4gp5vb.js"
   ],
   "/diaries": [
-    "static/chunks/0-lpz79ihinlq.js"
+    "static/chunks/16o_61azsd1uh.js"
   ],
   "/disclaimer": [
     "static/chunks/0egoa-8kh4qvf.js"
   ],
   "/posts": [
-    "static/chunks/0lbdch9p6yzpx.js"
+    "static/chunks/16v633m8~etlx.js"
   ],
   "/posts/[slug]": [
-    "static/chunks/17ez~7r9x7mhw.js"
+    "static/chunks/0wtngx~sodp6n.js"
   ],
   "/privacy-policy": [
     "static/chunks/17k~.6ay0k.3j.js"
